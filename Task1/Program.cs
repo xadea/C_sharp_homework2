2a780991bbee77b39301e790e66799455b3be58e
AnnaZimina <xadea@yandex.ru>
2022-08-09 21:49:15 +0300

    Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// программа, , которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Input three-digit number ");
+double num1 = Convert.ToInt32(Console.ReadLine());
+if(num1>99)
+{
+    if( num1<1000)
+    {
+        double num2 = ((num1-(num1 % 10))/10) % 10;
+        Console.WriteLine("The second digit in " + num1 + " is " + num2);
+    }
+    else Console.WriteLine("Wrong number");
+}
+else Console.WriteLine("Wrong number");
